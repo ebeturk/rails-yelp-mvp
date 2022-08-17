38ee7f496792b3a93933b4f81dcec487c652b71c
Emre Ebeturk <emreebeturk@gmail.com>
2022-08-17 16:59:27 +0200

    advanced restaurant close to finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+20.times do |n|
+  Restaurant.create!(
+  name: Faker::Hipster.word.capitalize,
+  address: Faker::Address.full_address,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: ["chinese", "italian", "japanese", "french", "belgian"].shuffle.first
+ )
+end
+
+puts "Finished!"
